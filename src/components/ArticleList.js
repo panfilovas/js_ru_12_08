93b9645dc2bf6dda983930aa13bbed66dff25ab2
Panfilov AS <panfilovas@8bit-tech.ru>
2016-09-02 22:37:10 +0300

    куча способов создания компонентов

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import {Component} from 'react'
 import Article from './Article'
 
+//export default Article as React.Component
+//export default Article as Component
+//const Article = React.createClass({
+//class Article extends Component
+//function Article(props) && export default connect()(Article)
+
 export default function ArticleList(props) {
-  //console.log(props.articles);
   const articleItems = props.articles.map(articleObject => <li key = {articleObject.id}><Article article = {articleObject}/></li>)
-  //console.log(articleItems)
+
   return (
     <ul>
       {articleItems}
